3bbc2d1a3cb9a2f1e5a446d5f032b26b3e4d3ae9
kauafardin <kauafardin2004@gmail.com>
2024-08-20 00:12:59 -0300

    Exercício aula 2

--- bubble_sort.py
+++ bubble_sort.py
@@ -5,13 +5,16 @@ def bolha(lista):
                 troca = lista[j]
                 lista[j] = lista[i]
                 lista[i] = troca
-    print(lista)
 
 lista = [5,1,3,4,2]
 bolha(lista)
-
+print("Aleatória: ",lista)
 lista = [5,4,3,2,1]
 bolha(lista)
-
+print("Inversa: ",lista)
 lista = [1,2,3,4,5]
 bolha(lista)
+print("Ordenada: ",lista)
+lista = [1,4,1,4,5]
+bolha(lista)
+print("Duplicados: ",lista)
